4ac2bb23d84efe4c9edfcfcce8eaadb2eb3098ca
子玖 <1359822154@qq.com>
2022-09-11 22:13:58 +0800

    feat: 导航栏

---
+++ resource-manager-system-ui/src/layout/Layout.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="head">
+      <div class="logo">
+        <div class="head-img">
+          <img src="@/assets/logo.svg" />
+        </div>
+        <div class="head-title">资源管理系统</div>
+      </div>
+      <div class="user-info-wrapper">
+        <div class="user-icon">
+          <img src="@/assets/user-icon.svg" />
+        </div>
+        <div class="user-name">小玟同学</div>
+      </div>
+    </div>
+    <div class="sidebar">
+      
+    </div>
+    <div class="main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  padding: 0 16px;
+
+  background: #0c193d;
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
+
+    .head-img > img {
+      height: 32px;
+      margin-right: 8px;
+    }
+    .head-title {
+      color: #6174fb;
+      font-size: 24px;
+      line-height: 24px;
+    }
+  }
+  .user-info-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    cursor: pointer;
+
+    .user-icon > img {
+      height: 16px;
+      width: 16px;
+      margin-right: 8px;
+    }
+    .user-name {
+      font-size: 16px;
+      line-height: 16px;
+    }
+  }
+}
+</style>
--- resource-manager-system-ui/src/views/Home.vue
+++ resource-manager-system-ui/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Layout>
+    <div class="home">首页</div>
+  </Layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Layout from "../layout/Layout.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Layout,
+  },
+};
 </script>
