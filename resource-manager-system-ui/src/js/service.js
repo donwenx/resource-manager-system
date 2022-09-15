40901363c0abf37264b29dfd1266691f37a55565
子玖 <1359822154@qq.com>
2022-09-15 22:47:40 +0800

    feat: 登录跳转逻辑

--- resource-manager-system-ui/src/js/service.js
+++ resource-manager-system-ui/src/js/service.js
@@ -1,6 +1,36 @@
 import { API, TOKEN } from '@/js/config';
 import axios from 'axios';
+import router from '@/router/index.js'
 
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  const token = window.localStorage.getItem(TOKEN);
+  if(token) {
+    config.headers.token = token || '';
+  }
+  console.log('config.headers.token', token);
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use((response) => {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据，token过期，或者无token时跳转登录
+  const code = response.data.code;
+  if(code === 1) {
+    router.push('/login');
+  }
+  console.log('response', response)
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 
 export const register = async (data) => {
   try {
@@ -37,11 +67,9 @@ export const login = async (data) => {
 
 export const upload = async (data) => {
   try {
-    const token = window.localStorage.getItem(TOKEN);
     const response = await axios({
       ...API.UPLOAD_FILE,
       data,
-      headers: { 'token': token },
     })
     return response.data;
   } catch (err) {
@@ -51,11 +79,9 @@ export const upload = async (data) => {
 
 export const resourceList = async (data) => {
   try {
-    const token = window.localStorage.getItem(TOKEN);
     const response = await axios({
       ...API.RESOURCE_LIST,
       params: data,
-      headers: { 'token': token },
     })
     return response.data.data;
   } catch (err) {
@@ -65,14 +91,13 @@ export const resourceList = async (data) => {
 
 export const resourceDownload = async (data) => {
   try {
-    // const token = window.localStorage.getItem(TOKEN);
     await axios({
       ...API.RESOURCE_DOWNLOAD,
       params: data,
       // headers: { 'token': token },
     })
     // return response.data;
-  } catch(err){
+  } catch (err) {
     console.error(err);
   }
 }
